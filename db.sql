02008ca520a226a01052ef9fdb3eff9aca6c6fca
Douglas Rubim <douglasrubims@gmail.com>
2020-08-06 05:14:34 -0300

    Removing old data

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE FinanceAppConfig(
-	id INTEGER UNIQUE NOT NULL AUTO_INCREMENT,
-	apiKey VARCHAR(255),
-	stockExchange VARCHAR(255),
-	expectedPrice FLOAT,
-	CONSTRAINT PK_FinanceAppConfig PRIMARY KEY(id)
-);
-
-INSERT INTO FinanceAppConfig (id, apiKey, stockExchange, expectedPrice) VALUES (1, 'YOUR_API_KEY', 'MSFT', 180.25);
